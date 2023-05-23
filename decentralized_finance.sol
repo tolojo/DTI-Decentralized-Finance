e465b2b58d1dfb851452e2c59265b8dc20a104dc
tolojo <tomasferreira91@gmail.com>
2023-05-23 01:52:09 +0100

    bye dex and sell dex, not tested sell dex

--- decentralized_finance.sol
+++ decentralized_finance.sol
@@ -1,26 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity >= 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract DecentralizedFinance is ERC20{
-    // TODO: define variables
 
+    uint256 rateETHtoDEX;
+    uint256 maxLoadDate;
+    struct Loan {         
+        uint256 deadline;         
+        uint256 amountEth;         
+        address lender;         
+        address borrower;         
+        bool isBaseNft;         
+        IERC721 nftContract;         
+        uint256 nftId;     
+        }
+    mapping(uint256 => Loan) public loans;
     event loanCreated(address indexed borrower, uint256 amount, uint256 deadline);
 
     constructor() ERC20("DEX", "DEX") {
         _mint(address(this), 10**30);
-
+        rateETHtoDEX = 50;
+        maxLoadDate = 30 days;
         // TODO: initialize
     }
 
     function buyDex() external payable {
-        // TODO: implement this
+        uint256 dexAmount = msg.value * rateETHtoDEX;
+        _mint(msg.sender, dexAmount);
     }
 
     function sellDex(uint256 dexAmount) external {
-        // TODO: implement this
+        require(balanceOf(msg.sender) >= dexAmount, "Insufficient DEX balance");
+        uint256 ethAmount = dexAmount / rateETHtoDEX;
+        address(this).transfer(dexAmount);
+        payable(msg.sender).transfer(ethAmount);
     }
 
     function loan(uint256 dexAmount, uint256 deadline) external {
@@ -38,7 +54,7 @@ contract DecentralizedFinance is ERC20{
     }
 
     function setRateEthToDex(uint256 rate) external {
-        // TODO: implement this
+        require(msg.sender==this.);
     }
 
     function getDex() public view returns (uint256) {
